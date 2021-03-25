f599ee1d056d9e858f29dbcfda93c1dd79ad1833
Nicole Martin <n.martinpdx@gmail.com>
2021-03-24 17:38:33 -0700

    corrected get add get route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,15 +10,16 @@ jest.mock('twilio', () => () => ({
   },
 }));
 
-describe('03_separation-of-concerns-demo routes', () => {
+describe('Lab 3 Route Tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
   let order;
   beforeEach(async () => {
-    order = await Order.insert({ quantity: 5 });
+    order = await Order.insert({ quantity: 10 });
   });
 
+  // POST TEST
   it('creates a new order in our database and sends a text message', () => {
     return request(app)
       .post('/api/v1/orders')
@@ -32,23 +33,25 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
 
-  it('ASYNC/AWAIT: gets all orders', async () => {
-    const res = await request(app).get('/api/v1/orders').send({ quantity: 10 });
+  // GET TEST
+  it('ASYNC/AWAIT: retrieves an order in our database', async () => {
+    const res = await request(app).get('/api/v1/orders');
 
-    expect(res.body).toEqual({
+    expect(res.body[0]).toEqual({
       id: '1',
-      quantity: 5,
+      quantity: 10,
     });
   });
 
-  // it('ets all orders', () => {
-  //   return request(app)
-  //     .get('/api/v1/orders')
-  //     .then((res) => {
-  //       expect(res.body).toEqual({
-  //         id: '1',
-  //         quantity: 10,
-  //       });
-  //     });
+  // // GET BY ID TEST
+  // it('gets one order by :id', async () => {
+  //   const res = await request(app)
+  //     .get('/api/v1/orders/:id')
+  //     .send({ quantity: 10 });
+
+  //   expect(res.body).toEqual({
+  //     id: '1',
+  //     quantity: 5,
+  //   });
   // });
 });
